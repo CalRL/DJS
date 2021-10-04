58518fc3ed45bb7b19ab1a3ae1fc109ecc90b00c
Cal <43823915+HuskiiRL@users.noreply.github.com>
2021-10-04 11:01:17 +0200

    v0.131
    code cleanup

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,21 +1,21 @@
-const Discord = require('discord.js')
-const args = message.content.split(' ')
-const errors = require('./errors.js')
+const Discord = require("discord.js");
+const args = message.content.split(" ");
+const errors = require("./errors.js");
 
 module.exports = {
-    names: ['avi', 'avatar'],
-    description: "Get a user's pfp",
-execute(message, args) {
-    if(args.length === 0) {
-    message.channel.send('You need to mention someone!')
-    return;
-    
+  names: ["avi", "avatar"],
+  description: "Get a user's pfp",
+  execute(message, args) {
+    if (args.length === 0) {
+      message.channel.send("You need to mention someone!");
+      return;
     } else {
-const user = message.mentions.users.first
-const aviEmbed = new Discord.MessageEmbed()
+      const user = message.mentions.users.first;
+      const aviEmbed = new Discord.MessageEmbed()
         .setAuthor(`${user.username}#${user.discriminator}`)
         .setColor("#000000")
-        .setImage(user.displayAvatarURL)
-        message.channel.send(aviEmbed)
+        .setImage(user.displayAvatarURL);
+      message.channel.send(aviEmbed);
     }
-}}
+  },
+};
--- commands/remindme.js
+++ commands/remindme.js
@@ -1,4 +1,4 @@
-const Discord = require('Discord.JS')
+const Discord = require("Discord.JS");
 
 /* 
 Pre requirements;
@@ -10,28 +10,29 @@ dateTo system.
 */
 
 module.exports = {
-    execute(message, args) {
+  execute(message, args) {
     var remindUser;
-    let userId = message.author.id
-    let timeNumber = args[0]
-    let unit = args[1]
-    var time = timeNumber*Unit
+    let userId = message.author.id;
+    let timeNumber = args[0];
+    let unit = args[1];
+    var time = timeNumber * Unit;
 
     var { months, month, mo } = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var { days, day , d } = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var { hours, hour, hrs, h } = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var { minutes, minute, mins, m } = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var { days, day, d } = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var { hours, hour, hrs, h } = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var { minutes, minute, mins, m } = Math.floor(
+      (distance % (1000 * 60 * 60)) / (1000 * 60)
+    );
 
-    if(!time || !unit) {
-        return
+    if (!time || !unit) {
+      return;
     } else {
-        var endDate = new Date(time)
-        var x = setInterval(function() {
-        if(distance < 0)
-        message.channel(userId).send("Timer is up!")
-        })
+      var endDate = new Date(time);
+      var x = setInterval(function () {
+        if (distance < 0) message.channel(userId).send("Timer is up!");
+      });
     }
-
-
-    }
-}
+  },
+};
